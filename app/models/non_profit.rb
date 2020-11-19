878a347bf7d72ca9ee9cf6c79ebc5036dea6a719
shazy89 <erdo.shazy123@gmail.com>
2020-11-19 10:17:09 -0500

    validations

--- app/models/non_profit.rb
+++ app/models/non_profit.rb
@@ -1,2 +1,9 @@
 class NonProfit < ApplicationRecord
+    validates :why, presence: true
+    validates :what_achieve, presence: true
+    validates :website, presence: true
+    validates :name, presence: true
+    validates :contact, presence: true
+    validates :contact_info, presence: true
+    
 end
